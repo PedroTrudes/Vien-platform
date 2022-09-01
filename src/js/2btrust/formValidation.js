b94f82e35427a2f8ffc6c8a10900798ec4f8e45e
PedroTrudes <pedro.jtrudes@gmail.com>
2022-08-31 21:58:42 -0300

    jogando dados na class

--- src/js/2btrust/formValidation.js
+++ src/js/2btrust/formValidation.js
@@ -1,38 +1,40 @@
 
-class Account  {
-  
-    constructor(user_id,
-        acknowledgment_signed, 
-        type, 
-        status, 
-        reinvest_yield, 
-        bank_name, 
-        bank_aba_routing_number,
-        bank_account_number,
-        accountHolder ){
-            this.user_id = user_id;
-            this.acknowledgment_signed = acknowledgment_signed;
-            this.type = type;
-            this.status = status;
-            this.reinvest_yield = reinvest_yield;
-            this.bank_name = bank_name;
-            this.bank_aba_routing_number = bank_aba_routing_number;
-            this.bank_account_number = bank_account_number;
-            this.accountHolder = accountHolder;
+class Account {
+
+  holders = [];
+  acknowledgment_signed ;
+    constructor(){
+        
+            this.user_id ="";
+            this.type ="" ;
+            this.status ="";
+            this.reinvest_yield="" ;
+            this.bank_name = "a";
+            this.bank_aba_routing_number = "a";
+            this.bank_account_number = "bank_account_number";
+            this.accountHolder = "accountHolder";
         }
+        //transformando dados em json
         toJson() {
             return JSON.stringify(this);
         }
 
-      
+        //inserindo dados no array
+        addAccountHolder(holders){
+            this.holders.push(holders);
+        }
 }
-let account = new Account(user_id, acknowledgment_signed,
-    type, status,
-    reinvest_yield, bank_name,
-    bank_aba_routing_number,bank_account_number,
-    accountHolder);
-    
+
+let account = new Account();
+//let holder = new Account();
 let jsonAccount = account.toJson();
+
+console.log(account);
+
+
+
+
+
 
 $(".classeDoFormulario")
 .validate({
@@ -70,15 +72,15 @@ $(".classeDoFormulario")
 }),
 ({
     submitHandler: form => {
-        let account = new Account();
-        account.user_id = document.getElementById("#");
-        account.acknowledgment_signed = document.getElementById("#");
-        account.type = document.getElementById("#");
-        account.status = document.getElementById("#");
-        account.reinvest_yield = document.getElementById("#");
-        account.bank_name = document.getElementById("#");
-        account.bank_aba_routing_number = document.getElementById("#");
-        account.bank_account_number = document.getElementById("#")
+        
+        account.user_id = $("#nomecampo").val();
+        account.acknowledgment_signed =  $("#nomecampo").val();
+        account.type =  $("#nomecampo").val();
+        account.status =  $("#nomecampo").val();
+        account.reinvest_yield =  $("#nomecampo").val();
+        account.bank_name =  $("#nomecampo").val();
+        account.bank_aba_routing_number =  $("#nomecampo").val();
+        account.bank_account_number =  $("#nomecampo").val();
         
         let jsonAccount = account.toJson();
         
@@ -87,34 +89,45 @@ $(".classeDoFormulario")
 })
 
 
-class AccountHolder{
-    constructor(account_id, fristname, middle_name,lastname, date_of_birth,
-        social_security_tax_id, country_citizenship,country_residency,country_tax_residency,
-        is_primary ){
-            this.account_id = account_id;
-            this.fristname = fristname;
-            this.middle_name = middle_name;
-            this.lastname = lastname;
-            this.date_of_birth = date_of_birth;
-            this.social_security_tax_id = social_security_tax_id;
-            this.country_citizenship = country_citizenship;
-            this.country_residency = country_residency;
-            this.country_tax_residency = country_tax_residency;
-            this.is_primary = is_primary;
+class AccountHolder {
+  account_id = 0;
+  is_primary = true;
+  address =[];
+    constructor(fristname){
+          
+            this.fristname;
+            this.middle_name = "middle_name";
+            this.lastname = "lastname";
+            this.date_of_birth = "date_of_birth";
+            this.social_security_tax_id = "social_security_tax_id";
+            this.country_citizenship = "country_citizenship";
+            this.country_residency = "country_residency";
+            this.country_tax_residency = "country_tax_residency";
         }
 
         toJson() {
             return JSON.stringify(this);
         }
 
-}
+        addAccountAddrress(address){
+            this.address.push(address);
+        }
+    }
 
-let accountHolder = new AccountHolder(account_id, fristname, middle_name,lastname, date_of_birth,
-    social_security_tax_id, country_citizenship,country_residency,country_tax_residency,
-    is_primary);
-let jsonAccountHolder = accountHolder.toJson();
+    
+    // teste de inserção de algum dado
+    let accountHolder = new AccountHolder();
+    accountHolder.fristname = "Pedro";
+    accountHolder.middle_name = "trudes";
+    //let addressHolder = new AccountHolder();
 
-$(".classeDoFormulario")
+    account.addAccountHolder(accountHolder);
+    
+    let jsonAccountHolder = accountHolder.toJson();
+    console.log(accountHolder);
+   
+
+$(".frmAccountHolder")
 .validate({
     rules:{
         account_id: {
@@ -152,62 +165,64 @@ $(".classeDoFormulario")
 }),
 ({
     submitHandler: form => {
-        let accountHolder = new AccountHolder();
-        accountHolder.account_id = document.getElementById("#");
-        accountHolder.fristname = document.getElementById("#");
-        accountHolder.middle_name = document.getElementById("#");
-        accountHolder.lastname = document.getElementById("#");
-        accountHolder.date_of_birth = document.getElementById("#");
-        accountHolder.social_security_tax_id = document.getElementById("#");
-        accountHolder.country_citizenship = document.getElementById("#");
-        accountHolder.country_residency = document.getElementById("#");
-        accountHolder.country_tax_residency = document.getElementById("#");
-        accountHolder.is_primary = document.getElementById("#");
+       
+       
+        accountHolder.account_id =  $("#namecampo").val();
+        accountHolder.fristname =  $("#firstName").val();
+        accountHolder.middle_name =  $("#middleName").val();
+        accountHolder.lastname =  $("#lastName").val();
+        accountHolder.date_of_birth =  $("#dateOfBirth").val();
+        accountHolder.social_security_tax_id =  $("#socialSecurityTaxId").val();
+        accountHolder.country_citizenship =  $("#countryCitizenship").val();
+        accountHolder.country_residency =  $("#countryResidency").val();
+        accountHolder.country_tax_residency =  $("#countryTaxResidency").val();
+        accountHolder.is_primary =  $("#nomecampo").val();
         
-        let jsonAccountHolder = accountHolder.toJson();
-        
-        console.log(jsonAccountHolder);
     }
 })
 
+
 class Address {
 
-    constructor(address, complement, city, state_id, zipcode, type){
-        this.address = address;
-        this.complement = complement;
-        this.city = city;
-        this.state_id = state_id;
-        this.zipcode = zipcode;
-        this.type = type;
+    constructor(){
+        this.address = "address";
+        this.complement = "complement";
+        this.city = "city";
+        this.state_id = "state_id";
+        this.zipcode = "zipcode";
+        this.type = "type";
     }
     toJson(){
         return JSON.stringify(this);
     }
     
 }
-let address = new Address(address, complement, city, state_id, zipcode, type);
+let address = new Address();
+accountHolder.addAccountAddrress(address);
 let jsonAddress = accountHolder.toJson();
 
-$(".classeDoFormulario")
+console.log(address);
+
+$(".frmAddress")
 .validate({
     rules:{
         address: {
-            require: true,
+            require: false,
         },
         complement: {
-            require: true,
+            require: false,
         },
         city:{
-            require: true,
+            require: false,
         },
         state_id: {
-            require: true,
+            require: false,
         },
         zipcode: {
-            require: true,
+            require: false,
         },
         type: {
-            require: true,
+            require: false,
         }
 
     }
@@ -215,30 +230,33 @@ $(".classeDoFormulario")
 ({
     submitHandler: form => {
         let address = new Address();
-        address.address = document.getElementById("#");
-        address.complement = document.getElementById("#");
-        address.city = document.getElementById("#");
-        address.state_id = document.getElementById("#");
-        address.zipcode = document.getElementById("#");
-        address.type = document.getElementById("#");
+
+        address.address =  $("#address").val();
+        address.complement =  $("#complement").val();
+        address.city =  $("#city").val();
+        address.state_id =  $("#stateId").val();
+        address.zipcode =  $("#zipcode").val();
+        address.type =  $("#nomecampo").val();
         
         let jsonAddress = address.toJson();
+
         
         console.log(jsonAddress);
     }
 })
 
 class Document {
-    constructor(name, file){
-        this.name = name;
-        this.file = file;
+    constructor(){
+        this.name = "name";
+        this.file = "file";
     }
     toJson(){
         return JSON.stringify(this);
     }
 }
 
-let documentUpload = new Document(name, file);
+let documentUpload = new Document();
+console.log(documentUpload);
 let jsonDocument = documentUpload.toJson();
 
 $(".classeDoFormulario")
@@ -255,11 +273,11 @@ $(".classeDoFormulario")
 ({
     submitHandler: form => {
         let documentUpload = new Document();
-        documentUpload.name = document.getElementById("#");
-        documentUpload.file = document.getElementById("#");
+        documentUpload.name = $("#").val();
+        documentUpload.file = $("#").val();
         
         let jsonDocumentUpload = documentUpload.toJson();
         
-        console.log(jsonDocumentUpload);
+        //console.log(jsonDocumentUpload);
     }
 })
